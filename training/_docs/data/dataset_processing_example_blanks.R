64c0b5cfc16dcbc4622d459c88916e8bce2f8ae6
Daphne Virlar-Knight <virlar-knight@nceas.ucsb.edu>
2022-09-09 08:00:55 -0700

    Successfully rendered all the training files

---
+++ training/_docs/data/dataset_processing_example_blanks.R
@@ -0,0 +1,40 @@
+###### The Setup ###########
+
+# Reminder Get test server token
+
+library(dataone)
+library(datapack)
+library(digest)
+library(uuid)
+library(dataone)
+library(arcticdatautils)
+library(EML)
+
+d1c <- D1Client("____________", "____________")
+packageId <- "____________"
+dp <- getDataPackage(____________, identifier=____________, lazyLoad=TRUE, quiet=FALSE)
+
+#get metadata id
+metadataId <-____________(dp, name="sysmeta@formatId", value="https://eml.ecoinformatics.org/eml-2.2.0")
+
+#edit the metadata
+doc <- ____________(getObject(____________, ____________))
+
+#add the publisher info
+doc <- eml_add_publisher(doc)
+doc <- eml_add_entity_system(doc)
+
+#format award info
+doc$dataset$project <- ____________("____________")
+
+#check and save the metadata
+____________(____________)
+eml_path <- arcticdatautils::title_to_file_name(doc$dataset$title)
+____________(doc, ____________h)
+
+dp <- replaceMember(____________, ____________, replacement=____________)
+
+#upload the dataset
+myAccessRules <- data.frame(subject="____________", permission="changePermission")
+packageId <- uploadDataPackage(d1c, dp, public=F, accessRules=myAccessRules, quiet=FALSE)
+#datamgmt::categorize_dataset("doi", c("theme1"), "Jasmine")
---
+++ training/_docs/data/dataset_processing_example_skeleton.R
@@ -0,0 +1,37 @@
+###### The Setup ###########
+
+# Reminder Get test server token
+
+library(dataone)
+library(datapack)
+library(digest)
+library(uuid)
+library(dataone)
+library(arcticdatautils)
+library(EML)
+
+#get the nodes
+
+
+#get the package
+
+
+#get metadata id
+
+
+#edit the metadata
+
+
+#add the publisher info
+
+
+#format award info
+
+
+#check and save the metadata
+
+
+
+#upload the dataset
+
+#datamgmt::categorize_dataset("doi", c("theme1"), "Jasmine")
